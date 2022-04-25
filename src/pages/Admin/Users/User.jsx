ee5651d4a5cc6e3886f4cab3f56f8a2b35f8ffba
dante-sarmiento <dante.sarmientof5@gmail.com>
2022-04-25 18:53:40 -0300

    UpdateUser

--- src/pages/Admin/Users/User.jsx
+++ src/pages/Admin/Users/User.jsx
@@ -1,4 +1,4 @@
-import { Typography } from 'antd'
+import { Modal, Typography } from 'antd'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 // import { UserList } from './UserListAntd/UserList'
@@ -6,6 +6,9 @@ import { ListaUsuarios } from './UserListAntd/UserList'
 
 export const User = () => {
   const [users, setUsers] = useState([]);
+  const [actionDialog, toggleActionDialog] = useState(false);
+  const [dialogMessage, setDialogMessage] = useState('');
+  const [dialogTitle, setDialogTitle] = useState('');
 
   async function loadUsers() {
     const res = await axios.get('http://localhost:3000/api/users');
@@ -13,24 +16,44 @@ export const User = () => {
     setUsers(res.data.users)
   }
 
-  
+  const handleDeleteUser = async (id) => {
+    try{
+      console.log(id)
+    const deletedUser = await axios.delete(`http://localhost:3000/api/user/${id}`)
+    const u = users.filter(user => user._id !== id);
+    setUsers(u)
+    setDialogMessage(deletedUser.data.msg);
+      setDialogTitle(`USUARIO ${id} ELIMINADO`)
+      toggleActionDialog(true)
+    }
+    catch (error){
+    console.log(error)
+    setDialogMessage('Error al borrar el usuario');
+      setDialogTitle(`ERROR`)
+      toggleActionDialog(true)
+    }
+  }
+
+  const UpdateUser = async (id) => {
+    console.log(id)
+  }
+
 
   useEffect(() => {
     loadUsers();
   }, []);
-
-  const handleDeleteUser = async (id) => {
-    // const deletedUser = await axios.delete()
-    const u = users.filter(user => user._id !== id);
-    setUsers(u)
-  }
+  
+  const hiddeModal = () => toggleActionDialog(false)
 
   
   return (
       <>
       <Typography.Title level={1}>Usuarios</Typography.Title>
         {/* <UserList/> */}
-        <ListaUsuarios functionDelete={handleDeleteUser} users={users}/>
+        <ListaUsuarios functionDelete={handleDeleteUser} changeStatus={UpdateUser} users={users}/>
+        <Modal title={dialogTitle} visible={actionDialog} onOk={hiddeModal} onCancel={hiddeModal}>
+        {dialogMessage}
+      </Modal>
       
       
       </>
--- src/pages/Admin/Users/UserListAntd/UserList.jsx
+++ src/pages/Admin/Users/UserListAntd/UserList.jsx
@@ -1,18 +1,31 @@
-import { List, Skeleton,Checkbox } from 'antd'
-import axios from 'axios'
+import { Checkbox, Button, Modal, Input } from 'antd'
 import { useEffect, useState } from 'react'
 import { UsersFromDB } from '../../../../constants/usersFromDB'
 import { UserListItem } from './userListItem/UserListItem'
 import { Table, Space  } from 'antd';
+import { UpdateUser } from '../UpdateUser/UpdateUser'
 const { Column } = Table
 
 
 
 
-export const ListaUsuarios = ({functionDelete, users}) => {
-  
+export const ListaUsuarios = ({functionDelete, changeStatus , users}) => {
 
   
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
   
 
   // function onChange(pagination, filters, sorter, extra) {
@@ -33,12 +46,27 @@ export const ListaUsuarios = ({functionDelete, users}) => {
             <Space size="middle">
               <Checkbox
                 checked={active}
-                // onChange={changeStatus}
+                
               >
               </Checkbox>
-              <a onClick={() => functionDelete(user._id)}>Delete </a>
+              <Button type='primary' danger onClick={() => functionDelete(user._id)}>Eliminar Usuario </Button>
+              <Button type="primary" onClick={() => changeStatus(user._id) } >
+                Editar Usuario
+            </Button>
+            <Modal title="Editar Usuario" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                <label htmlFor="">Nombre y Apellido</label>
+                <Input key={`${user.fullName}`} />
+                <label htmlFor="">Email</label>
+                <Input placeholder="Email" />
+                <label htmlFor="">Rol</label>
+                <Input placeholder="Rol" />
+                <label htmlFor="">Estado</label>
+                <Input placeholder="Estado" />
+            </Modal>
             </Space>
+            
           )}
+          
         />
       </Table>;
 
